48c4a9f908cc75c256ce91323076b6c2b7a867bf
Gabrielle Unsworth <gabrielle.unsworth@gmail.com>
2017-02-20 11:51:11 -0800

    Initial commit

--- Speech/main.c
+++ Speech/main.c
@@ -7,9 +7,63 @@
 //
 
 #include <stdio.h>
+#include "../../portaudio/include/portaudio.h"
+
+void logPaError(PaError err) {
+    printf("PortAudio error: %s\n", Pa_GetErrorText(err));
+    return;
+}
+
+// Begin example code here
+typedef struct
+{
+    float left_phase;
+    float right_phase;
+}
+paTestData;
+/* This routine will be called by the PortAudio engine when audio is needed.
+ It may called at interrupt level on some machines so don't do anything
+ that could mess up the system like calling malloc() or free().
+ */
+static int patestCallback( const void *inputBuffer, void *outputBuffer,
+                          unsigned long framesPerBuffer,
+                          const PaStreamCallbackTimeInfo* timeInfo,
+                          PaStreamCallbackFlags statusFlags,
+                          void *userData )
+{
+    /* Cast data passed through stream to our structure. */
+    paTestData *data = (paTestData*)userData;
+    float *out = (float*)outputBuffer;
+    unsigned int i;
+    (void) inputBuffer; /* Prevent unused variable warning. */
+    
+    for( i=0; i<framesPerBuffer; i++ )
+    {
+        *out++ = data->left_phase;  /* left */
+        *out++ = data->right_phase;  /* right */
+        /* Generate simple sawtooth phaser that ranges between -1.0 and 1.0. */
+        data->left_phase += 0.01f;
+        /* When signal reaches top, drop back down. */
+        if( data->left_phase >= 1.0f ) data->left_phase -= 2.0f;
+        /* higher pitch so we can distinguish left and right. */
+        data->right_phase += 0.03f;
+        if( data->right_phase >= 1.0f ) data->right_phase -= 2.0f;
+    }
+    return 0;
+}
+// End example code
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
+    PaError err = Pa_Initialize();
+    if (err != paNoError) {
+        logPaError(err);
+        return 1;
+    }
     printf("Hello, World!\n");
+    
+    err = Pa_Terminate();
+    if (err != paNoError) {
+        logPaError(err);
+    }
     return 0;
 }
