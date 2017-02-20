274c4be28570454f468918db896d0ffebe3589b2
Gabrielle Unsworth <gunsworth@outwardinc.com>
2017-02-20 11:30:57 -0800

    Initial Commit

---
+++ Speech/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Speech
+//
+//  Created by Gabrielle Unsworth on 2/20/17.
+//  Copyright © 2017 Gabrielle Unsworth. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
